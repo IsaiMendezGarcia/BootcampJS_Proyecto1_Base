9b2d4428a17cbd62c2347d39e44c63c2311e386c
IsaiMendezGarcia <isaindie2804@hotmail.com>
2024-09-17 12:56:35 -0600

    se agregan caracteres a la contrasena y funcion limpiar

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 let cantidad = document.getElementById('cantidad'); //funcion "document" accede a todos los elm del la pagina "metodo getElementById" selecciona elementos por su ID
 let boton = document.getElementById('boton');
-const cadenaCaracteres = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz013456789';
+const cadenaCaracteres = 'ABCDEFGHIJKLMNÑOPQRSTUVWXYZabcdefghijklmnñopqrstuvwxyz013456789!@#$%^&*()_';
 let contrasena = document.getElementById('contrasena')
 
 function generar(){
@@ -16,16 +16,21 @@ function generar(){
             let caracterAleatorio = cadenaCaracteres[Math.floor(Math.random() * cadenaCaracteres.length)]; //en cada iteraccion se obtine char dif
   
     
-            password+=caracterAleatorio;
+            password+=caracterAleatorio; // se concatena e indexa el char enterior a la variable "password"
         }
         contrasena.value= password;
     }
 
 }
 
+function limpiar(){
+    let clearPass = document.getElementById('contrasena');   // Creamos variable y la igualamos al contenido de "contrasena" usando el metodo document.get...
+    clearPass.value = ' ';  // la nueva variable almacena el valor de "contrasena" y por medio de "nombrevariable.value" se asigna el vanor de '   ' (NULL)
+}
+
 
 // console.log(typeof texto);Metodo typeof para visualizar el tipo de dato en consola utilizando la funcion "console.log"
 // dentro del if se utiliza "numeroDigitado" ya que es la trasnformacion a int del elemnto cantidad que se obtuvo en la linea 1
 // Math.random genera un numero aleatorio entre 0 y 1
-//Math.floor Redodne los valores a numeros enteros
+//Math.floor Redondea los valores a numeros enteros
 // += concatena y agrega valores
